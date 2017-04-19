04c302c7310897dc61e284e4683dcdca35dc1cdb
Spencer Alves <impiaaa@gmail.com>
2017-04-19 04:56:00 +0000

    Imported from Dropbox domain hacker.py at Wed, 19 Apr 2017 04:56:00 +0000

--- domain hacker.py
+++ domain hacker.py
@@ -9,7 +9,6 @@ mindomainlen = min(map(len, domains))
 maxdomainlen = max(map(len, domains))
 
 words = open("/usr/share/dict/words").readlines()
-random.shuffle(words)
 
 historyfilename = "domainhistory.txt"
 
@@ -20,42 +19,44 @@ else:
     history = set()
     historyfile = open(historyfilename, 'w')
 
-for word in words:
-    random.shuffle(domains)
-    word = word.strip()
-    lword = re.sub("\\W", "", word.lower())
-    for domain in domains:
-        if lword.enswith(domain):
-            #row = []
-            #row.append(word)
-            domain = lword[:-i]+'.'+lword[-i:]
-            if domain in history: coninue
-            history.add(domain)
-            historyfile.write(domain+'\n')
-            historyfile.flush()
-            #row.append(domain)
-            '''exitCode = os.system("host -W 1 "+domain+"")
-            if exitCode == 0:
-                # If it has a host,
-                # the website already exists.
-                #row.append("Taken")
-                continue
-            elif exitCode in (1, 256):
-                #row.append("Available")
-                pass
-            else:
-                raise Exception("invalid exit code "+str(exitCode))'''
-            if whois.whois(domain).expiration_date is not None: continue
-            '''url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="+urllib.quote_plus(word)
-            print url
-            urldoc = urllib.urlopen(url)
-            result = json.loads(urldoc.read())
-            urldoc.close()
-            row.append(result["responseData"]["cursor"]["estimatedResultCount"])'''
-            #row.append(len(word))
-            #row.append(i)
-            #writer.writerow(row)
-            break
-    time.sleep(5*60)
+while True:
+    random.shuffle(words)
+    for word in words:
+        random.shuffle(domains)
+        word = word.strip()
+        lword = re.sub("\\W", "", word.lower())
+        for domain in domains:
+            if lword.enswith(domain):
+                #row = []
+                #row.append(word)
+                domain = lword[:-i]+'.'+lword[-i:]
+                if domain in history: coninue
+                history.add(domain)
+                historyfile.write(domain+'\n')
+                historyfile.flush()
+                #row.append(domain)
+                '''exitCode = os.system("host -W 1 "+domain+"")
+                if exitCode == 0:
+                    # If it has a host,
+                    # the website already exists.
+                    #row.append("Taken")
+                    continue
+                elif exitCode in (1, 256):
+                    #row.append("Available")
+                    pass
+                else:
+                    raise Exception("invalid exit code "+str(exitCode))'''
+                if whois.whois(domain).expiration_date is not None: continue
+                '''url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="+urllib.quote_plus(word)
+                print url
+                urldoc = urllib.urlopen(url)
+                result = json.loads(urldoc.read())
+                urldoc.close()
+                row.append(result["responseData"]["cursor"]["estimatedResultCount"])'''
+                #row.append(len(word))
+                #row.append(i)
+                #writer.writerow(row)
+                break
+        time.sleep(5*60)
 fout.close()
 words.close()
