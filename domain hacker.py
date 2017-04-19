c04a834b30a32bc87b92ed217e16866207ea75c0
Spencer Alves <impiaaa@gmail.com>
2017-04-19 04:49:41 +0000

    Imported from Dropbox domain hacker.py at Wed, 19 Apr 2017 04:49:41 +0000

--- domain hacker.py
+++ domain hacker.py
@@ -1,4 +1,4 @@
-import os, csv, urllib, sys, re
+import os, urllib, sys, re, random, time, whois, mastodon
 
 domainslist = urllib.urlopen("http://data.iana.org/TLD/tlds-alpha-by-domain.txt")
 domains = set(filter(lambda a: not a.startswith('#'), [line.strip().lower() for line in domainslist]))
@@ -7,29 +7,28 @@ domainslist.close()
 mindomainlen = min(map(len, domains))
 maxdomainlen = max(map(len, domains))
 
-words = open("/usr/share/dict/words")
+words = open("/usr/share/dict/words").readlines()
+random.shuffle(words)
 
-if os.path.exists(sys.argv[1]):
-    lastfile = open(sys.argv[1])
-    lines = lastfile.readlines()
-    if len(lines) > 0:
-        lastword = lines[-1][:lines[-1].find(',')]
-        for word in words:
-            if word.strip() == lastword: break
-    lastfile.close()
+historyfilename = "domainhistory.txt"
 
-fout = open(sys.argv[1], 'a')
-writer = csv.writer(fout)
+if os.path.exists(historyfile):
+    history = set([s.strip() for s in open(historyfilename)])
+    historyfile = open(historyfilename, 'a')
+else:
+    history = set()
+    historyfile = open(historyfilename, 'w')
+
 for word in words:
     word = word.strip()
     lword = re.sub("\\W", "", word.lower())
     for i in range(mindomainlen, min(len(lword)-1, maxdomainlen)):
         if lword[-i:] in domains:
-            row = []
-            row.append(word)
+            #row = []
+            #row.append(word)
             domain = lword[:-i]+'.'+lword[-i:]
-            row.append(domain)
-            exitCode = os.system("host -W 1 "+domain+"")
+            #row.append(domain)
+            '''exitCode = os.system("host -W 1 "+domain+"")
             if exitCode == 0:
                 # If it has a host,
                 # the website already exists.
@@ -39,15 +38,17 @@ for word in words:
                 #row.append("Available")
                 pass
             else:
-                raise Exception("invalid exit code "+str(exitCode))
+                raise Exception("invalid exit code "+str(exitCode))'''
+            if whois.whois(domain).expiration_date is not None: continue
             '''url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="+urllib.quote_plus(word)
             print url
             urldoc = urllib.urlopen(url)
             result = json.loads(urldoc.read())
             urldoc.close()
             row.append(result["responseData"]["cursor"]["estimatedResultCount"])'''
-            row.append(len(word))
-            row.append(i)
-            writer.writerow(row)
+            #row.append(len(word))
+            #row.append(i)
+            #writer.writerow(row)
+    time.sleep(5*60)
 fout.close()
 words.close()
